fbcac0c50ba981e6b9f7877c8a526a9747fe79da
kidman90 <kacprzak90@gmail.com>
2017-10-24 21:25:24 +0200

    09 Update React Application State from Form Input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TodoForm, TodoList } from './components/todo';
+import { addTodo, generateId } from './lib/todoHelpers';
 
 class App extends Component {
   constructor() {
@@ -15,6 +16,21 @@ class App extends Component {
       currentTodo: ''
     };
   }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    const newId = generateId();
+    const newTodo = {
+      id: newId,
+      name: this.state.currentTodo,
+      isComplete: false
+    };
+    const updatedTodos = addTodo(this.state.todos, newTodo);
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: ''
+    });
+  };
 
   handleInputChange = (evt) => {
     this.setState({
@@ -33,6 +49,7 @@ class App extends Component {
           <TodoForm
             handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
+            handleSubmit={this.handleSubmit}
           />
           <TodoList
             todos={this.state.todos}
