08aedd83ab8c57179028d5485f1531a1b037b8b6
kidman90 <kacprzak90@gmail.com>
2017-10-26 22:53:23 +0200

    18 Filter Data on Property Values

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -22,3 +22,14 @@ export const removeTodo = (list, id) => {
     ...list.slice(removeIndex + 1)
   ];
 };
+
+export const filterTodos = (list, route) => {
+  switch (route) {
+    case '/active':
+      return list.filter(todo => !todo.isComplete);
+    case '/complete':
+      return list.filter(todo => todo.isComplete);
+    default:
+      return list;
+  }
+};
